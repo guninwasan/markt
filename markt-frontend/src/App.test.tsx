9910001b3e29b3d3c3411ff04d95d1c48e34e7a2
Gunin Wasan <guninwasan@gmail.com>
2024-10-13 12:55:01 -0400

    added test for App

--- markt-frontend/src/App.test.tsx
+++ markt-frontend/src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render the app properly and Markt should be visible", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Markt/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
