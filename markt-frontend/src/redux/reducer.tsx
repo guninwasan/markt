7065dbbf447078b3a3d75c158a48afab1bfccbc7
Gunin Wasan <guninwasan@gmail.com>
2024-10-19 21:53:53 -0400

    reducers

--- markt-frontend/src/redux/reducer.tsx
+++ markt-frontend/src/redux/reducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userAuth from "./slices/user-auth-slice";
+import device from "./slices/device-slice";
 
 const reducer = combineReducers({
   userAuth,
+  device,
 });
 
 export { reducer };
