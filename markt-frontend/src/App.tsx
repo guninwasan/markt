349d44fcd7585fac49088e93cbc23ab838161c88
Gunin Wasan <guninwasan@gmail.com>
2024-10-19 22:14:50 -0400

    implement useIsMobile hook

--- markt-frontend/src/App.tsx
+++ markt-frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useIsMobile } from "./hooks";
 import { HomePage } from "./pages/home-page";
+import { useDispatch } from "react-redux";
+import { setIsMobile } from "./redux";
+
 function App() {
+  const { isMobile } = useIsMobile();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setIsMobile(isMobile));
+  }, [isMobile]);
+
   return (
     <>
       <HomePage />
