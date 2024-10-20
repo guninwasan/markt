c8aa1b04cfc70a42e71b8068324e426ab3129c55
Gunin Wasan <guninwasan@gmail.com>
2024-10-19 23:54:00 -0400

    styling

--- markt-frontend/src/components/product-listing-component/product-listing-component.tsx
+++ markt-frontend/src/components/product-listing-component/product-listing-component.tsx
@@ -1,86 +1,19 @@
 import { useIsMobile } from "../../hooks";
 import { ImageGallery } from "../image-gallery";
-import styled from "styled-components";
 import { useEffect, useState } from "react";
-
-const StyledDiv = styled.div<{ isMobile: boolean | null }>;
-
-const ProductListingContainer = StyledDiv`
-  display: flex;
-  flex-direction: ${({ isMobile }) => (isMobile ? "column" : "row")};
-  gap: 2rem;
-  padding: 2rem;
-`;
-
-const ProductImages = styled.div`
-  flex: 1;
-`;
-
-const ProductDetails = StyledDiv`
-  flex: 2;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-const TitleAndPriceContainer = StyledDiv`
-  display: flex;
-  flex-direction: ${({ isMobile }) => (isMobile ? "column" : "row")};
-  gap: 2rem;
-`;
-
-const TitleAndDescription = styled.div`
-  flex: 3;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-const PriceBox = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-`;
-
-const PriceText = styled.div`
-  font-weight: bold;
-  font-size: 2.5rem;
-`;
-
-const SellerInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-`;
-
-const SellerAvatar = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-`;
-
-const ProductSpecsContainer = styled.div`
-  margin-top: 2rem;
-  border: 1px solid #ddd;
-  padding: 1rem;
-  border-radius: 8px;
-  background-color: #f9f9f9;
-`;
-
-const BottomTab = styled.div<{ isVisible: boolean }>`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  background-color: white;
-  border-top: 1px solid #ddd;
-  padding: 1rem;
-  display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
-`;
+import {
+  ProductListingContainer,
+  ProductImages,
+  ProductDetails,
+  TitleAndPriceContainer,
+  TitleAndDescription,
+  PriceBox,
+  PriceText,
+  SellerInfo,
+  SellerAvatar,
+  ProductSpecsContainer,
+  BottomTab,
+} from "./product-listing-component.styles";
 
 const ProductListingComponent = () => {
   const { isMobile } = useIsMobile();
