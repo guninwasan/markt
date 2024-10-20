c2b6cb9149b590c936e7f2c1e878b52e6b155a27
Gunin Wasan <guninwasan@gmail.com>
2024-10-19 21:46:52 -0400

    user auth slice

---
+++ markt-frontend/src/redux/slices/user-auth-slice.tsx
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type UserAuthState = {
+  userID: number;
+  name: string;
+  email: string;
+  phone: string;
+  jwt: string;
+};
+
+const initialState: UserAuthState = {
+  userID: -1,
+  name: "",
+  email: "",
+  phone: "",
+  jwt: "",
+};
+
+const userAuthSlice = createSlice({
+  name: "userAuth",
+  initialState,
+  reducers: {
+    setUserID: (state, action) => {
+      state.userID = action.payload;
+    },
+    setName: (state, action) => {
+      state.name = action.payload;
+    },
+    setEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    setPhone: (state, action) => {
+      state.phone = action.payload;
+    },
+    setJWT: (state, action) => {
+      state.jwt = action.payload;
+    },
+  },
+});
+
+export const { setUserID, setName, setEmail, setPhone, setJWT } =
+  userAuthSlice.actions;
+
+export default userAuthSlice.reducer;
