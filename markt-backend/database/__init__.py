fbc66983e8b49ed12097c338648b81ce212e8421
Rishistav <rishistav@gmail.com>
2024-10-28 22:07:28 -0400

    PostgreSQL Integration Complete

--- markt-backend/database/__init__.py
+++ markt-backend/database/__init__.py
@@ -2,23 +2,14 @@ from pathlib import Path
 import os
 from flask import Flask
 from .db import init_db, db
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app_db(db_name=None, testing=False):
-    app = Flask(__name__)
+db = SQLAlchemy()
 
-    basedir = Path(__file__).resolve().parent
-    DATABASE = "backend.db" if db_name is None else str(db_name)
-    url = os.getenv("DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(DATABASE)}")
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = url
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['TESTING'] = testing
-
-    init_db(app)
-
-    from .models import User, Listing
+def init_db(app):
+    db.init_app(app)
 
     with app.app_context():
+        print("Creating all tables...")
         db.create_all()
-
-    return app
+        print("All tables created successfully.")
--- markt-backend/database/models.py
+++ markt-backend/database/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     utorid = db.Column(db.String(50), unique=True, nullable=False)
-    password_encryp = db.Column(db.String(100), nullable=False)
+    password_encryp = db.Column(db.String(1000), nullable=False)
     phone = db.Column(db.String(15), nullable=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     
