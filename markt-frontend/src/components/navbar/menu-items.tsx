9b4683dbedd05f8e6c00e8f46be2613564d768c4
Vedant Gupta <vedant20gupta@gmail.com>
2024-10-29 19:48:42 -0400

    Using navlink to switch tabs without having to reload the page

--- markt-frontend/src/components/navbar/menu-items.tsx
+++ markt-frontend/src/components/navbar/menu-items.tsx
@@ -1,5 +1,6 @@
 import { useIsMobile } from "../../hooks";
 import { Menu, MenuItem, MenuLink } from "./navbar.styles";
+import { NavLink } from "react-router-dom";
 
 const MenuItems = () => {
   const { isMobile } = useIsMobile();
@@ -7,21 +8,34 @@ const MenuItems = () => {
   return (
     <Menu isMobile={isMobile}>
       <MenuItem>
-        <MenuLink href="/" className="active">
+        <MenuLink as={NavLink} to="/" className={({ isActive }) => (isActive ? "active" : "")}>
           Home
         </MenuLink>
       </MenuItem>
       <MenuItem>
-        <MenuLink href="/">Buy</MenuLink>
+        <MenuLink as={NavLink} to="/buy" className={({ isActive }) => (isActive ? "active" : "")}>
+          Buy
+        </MenuLink>
       </MenuItem>
       <MenuItem>
-        <MenuLink href="/">Sell</MenuLink>
+        <MenuLink as={NavLink} to="/sell" className={({ isActive }) => (isActive ? "active" : "")}>
+          Sell
+        </MenuLink>
       </MenuItem>
       <MenuItem>
-        <MenuLink href="/">Login/Register</MenuLink>
+        <MenuLink as={NavLink} to="/wishlist" className={({ isActive }) => (isActive ? "active" : "")}>
+          Wishlist
+        </MenuLink>
+      </MenuItem>
+      <MenuItem>
+        <MenuLink as={NavLink} to="/register" className={({ isActive }) => (isActive ? "active" : "")}>
+          Login/Register
+        </MenuLink>
       </MenuItem>
     </Menu>
   );
 };
 
 export { MenuItems };
+
+// Used GPT to change from href to navlink
