6ecb9e28fff2fe63fa1e48c04b39b1147bbf7c7b
Shikhar <cshikhar15@gmail.com>
2024-10-31 01:43:51 -0400

    Added front end validation for registration and error handling from backend.

--- markt-frontend/src/errorCodes.ts
+++ markt-frontend/src/errorCodes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export enum ErrorRsp {
     OK = 1000,
     ERR_PARAM = 1001,
@@ -8,15 +7,4 @@ export enum ErrorRsp {
     ERR_PARAM_PHONE = 1005,
     ERR_NOT_FOUND = 1006,
     ERR = 1007,
-=======
-// script generated - do not modify.
-export enum ErrorRsp {
-    OK = 1000,
-    ERR_PARAM = 1001,
-    ERR_PARAM_EMAIL = 1002,
-    ERR_PARAM_PWD = 1003,
-    ERR_PARAM_PHONE = 1004,
-    ERR_NOT_FOUND = 1005,
-    ERR = 1006,
->>>>>>> 79b62b5 (Added front end validation and added error handling for response.)
 }
