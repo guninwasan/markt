5aaced5ebade0e2a044cfefd18afd290a7a8f12a
Gunin Wasan <guninwasan@gmail.com>
2024-11-01 19:20:38 -0400

    image gallery tests

---
+++ markt-frontend/src/components/image-gallery/image-gallery.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { ImageGallery } from "./image-gallery";
+
+const mediaUrls = [
+  "https://example.com/image1.jpg",
+  "https://example.com/image2.jpg",
+];
+
+describe("ImageGallery", () => {
+  it("should render thumbnails", () => {
+    render(<ImageGallery mediaUrls={mediaUrls} />);
+    expect(screen.getAllByRole("img")).toHaveLength(2);
+  });
+
+  it("should open modal on thumbnail click", () => {
+    render(<ImageGallery mediaUrls={mediaUrls} />);
+
+    fireEvent.click(screen.getAllByRole("img")[0]);
+    expect(screen.getByText("✕")).toBeInTheDocument();
+  });
+
+  it("should close modal on close button click", () => {
+    render(<ImageGallery mediaUrls={mediaUrls} />);
+
+    fireEvent.click(screen.getAllByRole("img")[0]);
+    fireEvent.click(screen.getByText("✕"));
+    expect(screen.queryByText("✕")).not.toBeInTheDocument();
+  });
+
+  it("should match snapshot", () => {
+    const { container } = render(<ImageGallery mediaUrls={mediaUrls} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it.only("should navigate to next image", () => {
+    render(<ImageGallery mediaUrls={mediaUrls} />);
+    fireEvent.click(screen.getAllByRole("img")[0]);
+
+    const nextButton = screen.getByTestId("next");
+    expect(nextButton).toBeInTheDocument();
+
+    const getFirstImageIndex = screen.getByText("1/2");
+    expect(getFirstImageIndex).toBeInTheDocument();
+    fireEvent.click(nextButton);
+
+    const getSecondImageIndex = screen.getByText("2/2");
+    expect(getSecondImageIndex).toBeInTheDocument();
+  });
+});
