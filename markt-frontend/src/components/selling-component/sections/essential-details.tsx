83e36dd48bae7c035cba410eb185c9533d5de882
Gunin Wasan <guninwasan@gmail.com>
2024-11-04 17:11:35 -0500

    added essential details

---
+++ markt-frontend/src/components/selling-component/sections/essential-details.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  FormGroup,
+  Label,
+  TextInput,
+  TextArea,
+  SectionHeader,
+} from "../selling-component.styles";
+
+const EssentialDetails = ({
+  formData,
+  handleChange,
+  handlePriceChange,
+  priceError,
+}: any) => (
+  <>
+    <SectionHeader>Essential Details</SectionHeader>
+    <FormGroup style={{ flex: "1 1 45%" }}>
+      <Label>Title *</Label>
+      <TextInput
+        type="text"
+        name="title"
+        required
+        placeholder="Enter the product title"
+        value={formData.title}
+        onChange={handleChange}
+      />
+    </FormGroup>
+    <FormGroup style={{ flex: "1 1 45%" }}>
+      <Label>Price (CAD) *</Label>
+      <TextInput
+        type="text"
+        name="price"
+        required
+        placeholder="Enter the product price"
+        value={formData.price}
+        onChange={handlePriceChange}
+      />
+      {priceError && <p style={{ color: "red" }}>{priceError}</p>}
+    </FormGroup>
+    <FormGroup>
+      <Label>Description *</Label>
+      <TextArea
+        name="description"
+        required
+        placeholder="Provide a detailed description of the item..."
+        value={formData.description}
+        onChange={handleChange}
+      />
+    </FormGroup>
+    <FormGroup>
+      <Label>
+        <input
+          type="checkbox"
+          name="negotiable"
+          checked={formData.negotiable}
+          onChange={handleChange}
+        />
+        Price Negotiable
+      </Label>
+    </FormGroup>
+  </>
+);
+
+export { EssentialDetails };
