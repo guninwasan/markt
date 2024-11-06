9cbf5624a9245cca32ed99c75702d4de27226b4f
Gunin Wasan <guninwasan@gmail.com>
2024-11-06 13:24:44 -0500

    update routes to make it protected

--- markt-frontend/src/App.tsx
+++ markt-frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HomePage, LoginPage, RegisterPage, SellPage, ProfilePage, NotFoundPage } from "./pages";
+import {
+  HomePage,
+  LoginPage,
+  RegisterPage,
+  SellPage,
+  ProfilePage,
+  NotFoundPage,
+} from "./pages";
 import { ProductListingPage } from "./pages";
 import { useIsMobile } from "./hooks";
+import { ProtectedRoute } from "./components";
 
 function App() {
   useIsMobile();
@@ -13,8 +21,14 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/listing" element={<ProductListingPage />} />
-        <Route path="/sell" element={<SellPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/sell"
+          element={<ProtectedRoute element={<SellPage />} />}
+        />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute element={<ProfilePage />} />}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
