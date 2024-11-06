743ee57d5e5794ea30696746dbaf8f9ec75d0a04
Gunin Wasan <guninwasan@gmail.com>
2024-11-06 16:41:09 -0500

    add search route

--- markt-frontend/src/App.tsx
+++ markt-frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HomePage, LoginPage, RegisterPage, SellPage, ProfilePage, NotFoundPage } from "./pages";
-import { ProductListingPage } from "./pages";
+import {
+  HomePage,
+  LoginPage,
+  RegisterPage,
+  SellPage,
+  ProfilePage,
+  NotFoundPage,
+  SearchPage,
+  ProductListingPage,
+} from "./pages";
 import { useIsMobile } from "./hooks";
 
 function App() {
@@ -15,6 +23,7 @@ function App() {
         <Route path="/listing" element={<ProductListingPage />} />
         <Route path="/sell" element={<SellPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
