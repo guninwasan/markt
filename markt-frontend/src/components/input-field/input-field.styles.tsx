32b6760fcb8f5a7acdf1e39c78d6dd8b4032444a
Gunin Wasan <guninwasan@gmail.com>
2024-11-10 23:48:55 -0500

    changed input field

--- markt-frontend/src/components/input-field/input-field.styles.tsx
+++ markt-frontend/src/components/input-field/input-field.styles.tsx
@@ -1,24 +1,43 @@
-const inputStyles = {
-    width: '29.625rem',
-    height: '2.625rem',
-    flexShrink: 0,
-    borderRadius: '0.3125rem',
-    background: '#D9D9D9',
-    boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-    padding: '0.5rem',
-    border: 'none',
-    fontSize: '1rem',
-  };
+import styled from "styled-components";
+import { colors } from "../../utils";
 
-export const loginButtonStyles = {
-    width: '19.9375rem',
-    height: '4.125rem',
-    flexShrink: 0,
-    borderRadius: '0.3125rem',
-    background: '#A7A7A7',
-    boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-    border: 'none', // Remove default button borders
-    fontSize: '1.25rem', // Adjust font size for the text in the button
-    cursor: 'pointer', // Change cursor on hover
-  };
-export { inputStyles };
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background: ${colors.lightGrey};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  padding: 0.5rem 1rem;
+  width: 100%;
+  border: none;
+  transition: background 0.3s ease;
+  height: 2.625rem;
+
+  &:focus {
+    background: ${colors.white};
+    outline: none;
+    box-shadow: 0px 0px 5px 2px ${colors.primary};
+  }
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 14px;
+  margin-top: 8px;
+`;
+
+export { Container, InputWrapper, Input, ErrorMessage };
