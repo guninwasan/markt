0630230ad3a46c761106f916f690685dfc8ea586
Gunin Wasan <guninwasan@gmail.com>
2024-11-11 14:31:02 -0500

    timeout only when loading

--- markt-frontend/src/components/loading-modal/loading-modal.tsx
+++ markt-frontend/src/components/loading-modal/loading-modal.tsx
@@ -6,29 +6,56 @@ import {
   TimerText,
   SupportLink,
 } from "./loading-modal.styles";
+import { useSelector } from "react-redux";
+import { RootState, selectors, setIsLoading } from "../../redux";
+import { useDispatch } from "react-redux";
 
 const LoadingModal = () => {
   const [message, setMessage] = useState<any>("");
+  const dispatch = useDispatch();
+
+  const { isLoading } = useSelector((state: RootState) => ({
+    isLoading: selectors.getIsLoading(state),
+  }));
+
+  const handleOnClick = (e: any) => {
+    e.preventDefault();
+    dispatch(setIsLoading(false));
+  };
 
   useEffect(() => {
-    const timer1 = setTimeout(() => {
-      setMessage("Things are taking longer than usual...");
-    }, 10000);
+    let timer1: NodeJS.Timeout;
+    let timer2: NodeJS.Timeout;
 
-    const timer2 = setTimeout(() => {
-      setMessage(
-        <>
-          Still no luck? Try contacting support{" "}
-          <SupportLink href="/support">here</SupportLink>.
-        </>
-      );
-    }, 20000);
+    if (isLoading) {
+      timer1 = setTimeout(() => {
+        setMessage("Things are taking longer than usual...");
+      }, 10000);
+
+      timer2 = setTimeout(() => {
+        setMessage(
+          <>
+            Still no luck? Try contacting support{" "}
+            <SupportLink href="/support" onClick={handleOnClick}>
+              here
+            </SupportLink>
+            .
+          </>
+        );
+      }, 20000);
+    } else {
+      setMessage("");
+    }
 
     return () => {
       clearTimeout(timer1);
       clearTimeout(timer2);
     };
-  }, []);
+  }, [isLoading]);
+
+  if (!isLoading) {
+    return null;
+  }
 
   return (
     <ModalBackground data-testid="modal-background">
