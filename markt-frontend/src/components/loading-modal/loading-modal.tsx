944aa5fadbac0c251d044c657d9cb8bb5e9a80ee
Gunin Wasan <guninwasan@gmail.com>
2024-11-11 14:17:57 -0500

    loading modal text and styles

--- markt-frontend/src/components/loading-modal/loading-modal.tsx
+++ markt-frontend/src/components/loading-modal/loading-modal.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   ModalBackground,
   LoadingIndicator,
   LoadingText,
+  TimerText,
+  SupportLink,
 } from "./loading-modal.styles";
 
 const LoadingModal = () => {
+  const [message, setMessage] = useState<any>("");
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setMessage("Things are taking longer than usual...");
+    }, 10000);
+
+    const timer2 = setTimeout(() => {
+      setMessage(
+        <>
+          Still no luck? Try contacting support{" "}
+          <SupportLink href="/support">here</SupportLink>.
+        </>
+      );
+    }, 20000);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []);
+
   return (
     <ModalBackground data-testid="modal-background">
       <LoadingIndicator data-testid="loading-indicator" />
       <LoadingText>Loading....</LoadingText>
+      {message && <TimerText>{message}</TimerText>}
     </ModalBackground>
   );
 };
